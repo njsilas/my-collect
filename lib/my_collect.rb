9a312bfaa07c2fb01ce0010d42d288d52bf8c0ac
Nicholas Silas <njsilas@gmail.com>
2020-10-26 19:05:29 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(x)
+    counter = 0
+    x_nu = []
+    while counter < x.length
+        x_val = yield(x[counter])
+        x_nu << x_val
+        counter += 1
+    end
+x_nu
+end
 
